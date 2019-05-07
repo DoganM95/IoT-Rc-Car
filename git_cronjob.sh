8cee6168d6aa95d6a2616c9edbebf4949555f4e4
DoganM95 <doganmermertas@hotmail.de>
2019-05-07 13:28:53 -0500

    autosaved all changes via chronjob on pi

--- git_cronjob.sh
+++ git_cronjob.sh
@@ -1,9 +1,9 @@
 echo hi;
 git --version;
 PROJECT_PATH="/home/pi/Project/RaspberryPi_RC-Car/";
-echo $PROJECT_PATH;
-git fetch $PROJECT_PATH;
-git pull $PROJECT_PATH;
-git add $PROJECT_PATH;
-git commit -m "autosaved all changes via chronjob on pi";
-git push $PROJECT_PATH;
+echo $PROJECT_PATH &&
+git fetch $PROJECT_PATH &&
+git pull $PROJECT_PATH &&
+git add $PROJECT_PATH &&
+git commit -m "autosaved all changes via chronjob on pi" &&
+git push $PROJECT_PATH
