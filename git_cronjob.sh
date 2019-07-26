92c416fb0f4605faa128738f640a7813a10630bb
DoganM95 <doganmermertas@hotmail.de>
2019-07-26 11:46:06 +0200

    deleted unnecessary scripts

--- git_cronjob.sh
+++
@@ -1,8 +0,0 @@
-echo hi &&
-git --version &&
-cd "/home/pi/Project/RaspberryPi_RC-Car/" &&
-git fetch &&
-git pull &&
-git add . &&
-git commit -m "autosaved all changes via chronjob on pi" &&
-git push;
