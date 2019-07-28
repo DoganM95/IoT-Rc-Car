2feb2c52a8304e3519b3c9531115a5475987fe2f
DoganM95 <doganmermertas@hotmail.de>
2019-07-28 17:53:23 -0500

    repaired shell script

--- src/main/shell/AutoGitPull.sh
+++ src/main/shell/AutoGitPull.sh
@@ -6,39 +6,30 @@ timestamp() {
 }
 
 
- ACTION='\033[1;90m'
- FINISHED='\033[1;96m'
- READY='\033[1;92m'
- NOCOLOR='\033[0m' # No Color
- ERROR='\033[0;31m'
+cd /home/pi/project/RaspberryPi_RC-Car/
+git checkout master
 
 while :; do
-    cd /home/pi/Project/RaspberryPi_RC-Car/
-    pwd
-    git checkout master
-    git fetch   ; echo "exit code: ${?}"
+    git fetch
     UPSTREAM=${1:-'@{u}'}
     LOCAL=$(git rev-parse @)
     REMOTE=$(git rev-parse "$UPSTREAM")
     BASE=$(git merge-base @ "$UPSTREAM")
 
     if [ $LOCAL = $REMOTE ]; then
-        echo $(timestamp) "Up-to-date" 
-    elif [ $LOCAL = $BASE ]; then
-        echo $(timestamp) "Need to pull"
+        echo $(timestamp) "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+        echo $(timestamp) "Pulling changes from remote.."
         git pull
     elif [ $REMOTE = $BASE ]; then
-        echo $(timestamp) "Need to push"
-    else
+        echo $(timestamp) "Pushing local changes to remote.."
+	git pull
+	git add .
+	git commit -m "Automatic push of changes"
+	git push
+else
         echo $(timestamp) "Diverged"
     fi
-
-if git diff-index --quiet HEAD --; then
-    # No changes
-    echo "third script managed to find changes"
-else
-    # Changes
-    echo "third script found no changes"
-fi
     sleep 1
+    echo 
 done
