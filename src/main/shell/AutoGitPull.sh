676500a8ca81f3398e8d819728c2cab77f9a9783
DoganM95 <doganmermertas@hotmail.de>
2019-07-30 05:56:06 -0500

    further testing for auto restart

--- src/main/shell/AutoGitPull.sh
+++ src/main/shell/AutoGitPull.sh
@@ -15,25 +15,6 @@ git fetch
 git checkout master
 git pull
 #sudo npm start &
-zenity --info --text "startet server" &
-#xterm -e 'bash -c "echo server is up;sleep 10"' &>/dev/null &
-
-#Then start a script to poll and pull on changes and restart server afterwards
-#Script will run free from hangups (nohup)
-#nohup /home/pi/project/RaspberryPi_RC-Car/src/main/shell/AutoGitPull.sh &
-#sudo nohup /home/pi/Project/RaspberryPi_RC-Car/src/main/shell/AutoGitPull.sh >$
-
-
-
-
-#start RC-Car Server initially on boot
-echo "another run" >> /home/pi/Desktop/ran.txt
-cd /home/pi/project/RaspberryPi_RC-Car/
-sudo killall node
-git fetch
-git checkout master
-git pull
-#sudo npm start &
 sudo npm run $NPMRUN &
 zenity --info --text "Startet Server" &
 #xterm -e 'bash -c "echo server is up;sleep 10"' &>/dev/null &
@@ -47,8 +28,6 @@ zenity --info --text "Startet Server" &
 
 
 cd /home/pi/project/RaspberryPi_RC-Car/
-git checkout master
-
 while :; do
     git fetch
     UPSTREAM=${1:-'@{u}'}
@@ -58,11 +37,11 @@ while :; do
 
     if [ $LOCAL = $REMOTE ]; then
         echo $(timestamp) "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
+    elif [ $LOCAL = $BASE ]; then
         echo $(timestamp) "Pulling changes from remote.."
 	sudo killall node
 	git pull
-	sudo npm run $NPMRUN
+	sudo npm run $NPMRUN &
     elif [ $REMOTE = $BASE ]; then
         echo $(timestamp) "Pushing local changes to remote.."
 	git pull
