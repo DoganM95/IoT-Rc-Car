26197ebc81665416097b588c2bd0a7cec363fc39
DoganM95 <doganmermertas@hotmail.de>
2019-08-01 20:43:07 +0200

    test run after a long time

--- src/main/javascript/webserver.js
+++ src/main/javascript/webserver.js
@@ -10,14 +10,26 @@ let three = require("three"); //https://www.npmjs.com/package/three //
 //Variables
 //-----------------------------------------------------------------------------
 //GPIO Objects
-let motorLeft = new pigpio(4, { mode: pigpio.OUTPUT }); //use GPIO pin 4 as output
-let motorRight = new pigpio(4, { mode: pigpio.OUTPUT }); //use GPIO pin 4 as output
+let motorLeft = new pigpio(4, {
+  mode: pigpio.OUTPUT
+}); //use GPIO pin 4 as output
+let motorRight = new pigpio(4, {
+  mode: pigpio.OUTPUT
+}); //use GPIO pin 4 as output
 
-let servoLeft = new pigpio(23, { mode: pigpio.OUTPUT });
-let servoRight = new pigpio(24, { mode: pigpio.OUTPUT });
+let servoLeft = new pigpio(23, {
+  mode: pigpio.OUTPUT
+});
+let servoRight = new pigpio(24, {
+  mode: pigpio.OUTPUT
+});
 
-let engineLeftSpeed = new pigpio(17, { mode: pigpio.OUTPUT });
-let engineRightSpeed = new pigpio(16, { mode: pigpio.OUTPUT });
+let engineLeftSpeed = new pigpio(17, {
+  mode: pigpio.OUTPUT
+});
+let engineRightSpeed = new pigpio(16, {
+  mode: pigpio.OUTPUT
+});
 
 let motors = [motorLeft, motorRight]; //Control all motors at once
 let servos = [servoLeft, servoRight]; //Control all servos at once
@@ -37,61 +49,62 @@ let client;
 //     }, 1);
 //   }
 // '
-  //Reset Pins to low - Init
-  // servos.writeServosProto();
-  // steering.writeServos(1500);
-  // steering.leftServo.servoWrite(1000);
-  // steering.rightServo.servoWrite(1500);
-  motorLeft.pwmWrite(0);
+//Reset Pins to low - Init
+// servos.writeServosProto();
+// steering.writeServos(1500);
+// steering.leftServo.servoWrite(1000);
+// steering.rightServo.servoWrite(1500);
+motorLeft.pwmWrite(0);
 
-  //Server Socket listener
-  io.sockets.on("connection", function(socket) {
-    // WebSocket Connection
+//Server Socket listener
+io.sockets.on("connection", function (socket) {
+  // WebSocket Connection
 
-    socket.on("connection"),
-      identityObject => {
-        client = identityObject;
-      };
+  socket.on("connection"),
+    identityObject => {
+      client = identityObject;
+    };
 
-    socket.on("motorLeftSocket", data => {
-      //when motorLeftSocket receives data from client
-      motorLeft.pwmWrite(data); //set motorLeft-speed
-      socket.emit("motorLeftSpeedSocket", motorLeft.getPwmDutyCycle()); //emit motorLeftSpeed to client-side to keep client updated about motorLefts current speed
-    });
-
-    socket.on("servoSocket", data => {
-      //entry point for servo writes
-
-      //TODO: place algorithm here to sync servo angles
-      steering.leftServo.servoWrite(data);
-      steering.rightServo.servoWrite(data);
-
-      socket.emit(
-        "leftServoAngleSocket",
-        steering.leftServo.getServoPulseWidth()
-      );
-      socket.emit(
-        "rightServoAngleSocket",
-        steering.rightServo.getServoPulseWidth()
-      );
-    });
-
-    // motorLeft.pwmRange(30);
+  socket.on("motorLeftSocket", data => {
+    //when motorLeftSocket receives data from client
+    motorLeft.pwmWrite(data); //set motorLeft-speed
+    socket.emit("motorLeftSpeedSocket", motorLeft.getPwmDutyCycle()); //emit motorLeftSpeed to client-side to keep client updated about motorLefts current speed
   });
 
-  //Server
-  http.listen(8080); //listen to port 8080
-  console.log("waiting for connection on web-interface.");
+  socket.on("servoSocket", data => {
+    //entry point for servo writes
 
-  //Event Listener
-  process.on("SIGINT", function() {
-    //on ctrl+c
-    console.log("killing server.");
-    motorLeft.pwmWrite(0); // Turn motorLeft off
-    steering.leftServo.servoWrite(1500);
-    steering.rightServo.servoWrite(1500); // center steering
-    process.exit(); //exit completely
-  });'
+    //TODO: place algorithm here to sync servo angles
+    steering.leftServo.servoWrite(data);
+    steering.rightServo.servoWrite(data);
+
+    socket.emit(
+      "leftServoAngleSocket",
+      steering.leftServo.getServoPulseWidth()
+    );
+    socket.emit(
+      "rightServoAngleSocket",
+      steering.rightServo.getServoPulseWidth()
+    );
+  });
+
+  // motorLeft.pwmRange(30);
+});
+
+//Server
+http.listen(8080); //listen to port 8080
+console.log("waiting for connection on web-interface.");
+
+//Event Listener
+process.on("SIGINT", function () {
+  //on ctrl+c
+  console.log("killing server.");
+  motorLeft.pwmWrite(0); // Turn motorLeft off
+  steering.leftServo.servoWrite(1500);
+  steering.rightServo.servoWrite(1500); // center steering
+  process.exit(); //exit completely
+});
+
 // })();
 
 //-----------------------------------------------------------------------------
@@ -135,7 +148,7 @@ const engine = {
 //-----------------------------------------------------------------------------
 //METHODS
 //-----------------------------------------------------------------------------
-Array.prototype.writeMotorsProto = function(speed) {
+Array.prototype.writeMotorsProto = function (speed) {
   this.forEach(motor => {
     if (speed > 100) {
       motor.pwmWrite(100);
@@ -147,7 +160,7 @@ Array.prototype.writeMotorsProto = function(speed) {
   });
 };
 
-Array.prototype.writeServosProto = function(steering) {
+Array.prototype.writeServosProto = function (steering) {
   this.forEach(servo => {
     if (steering > 2500) {
       servo.servoWrite(2500);
@@ -164,16 +177,20 @@ Array.prototype.writeServosProto = function(steering) {
 //-----------------------------------------------------------------------------
 function handler(req, res) {
   //create server
-  fs.readFile(__dirname + "/index.html", function(err, data) {
+  fs.readFile(__dirname + "/index.html", function (err, data) {
     //read file index.html in public folder
     if (err) {
       //error-handling
-      res.writeHead(404, { "Content-Type": "text/html" }); //display 404 on error
+      res.writeHead(404, {
+        "Content-Type": "text/html"
+      }); //display 404 on error
       res.end("404 Not Found");
       return;
     } else {
       //just for readability
-      res.writeHead(200, { "Content-Type": "text/html" }); //write HTML
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      }); //write HTML
       res.write(data); //write data from index.html
       res.end();
       return;
