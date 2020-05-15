4ea6214eee985b1d2342a55a17c0453f77b64a57
DoganM95 <doganmermertas@hotmail.de>
2020-05-15 15:39:09 +0200

    testing exported functions

---
+++ RaspberryPi/Software/src/main/javascript/client/controls/keypad.js
@@ -0,0 +1,38 @@
+export async function handler(key) {
+  console.log("Key: " + key.keyCode + " pressed."); //log the pressed key
+  switch (
+    key.keyCode //switch statement for pressed key
+  ) {
+    case 40: //backward
+      if (thisClient.car.engine.speed.value > 0) {
+        socket.emit("motorSocket", thisClient.car.engine.speed.value - thisClient.car.engine.speed.increment);
+      }
+      break;
+    case 38: //forward
+      if (thisClient.car.engine.speed.value < 255) {
+        socket.emit("motorSocket", thisClient.car.engine.speed.value + thisClient.car.engine.speed.increment);
+      }
+      break;
+    case 53: //Full Speed or stop (like play pause)
+      if (thisClient.car.engine.speed.value > 0) {
+        socket.emit("motorSocket", 0); //stops motor, if motor is running
+      }
+      if (thisClient.car.engine.speed.value == 0) {
+        socket.emit("motorSocket", 255); //max-speeds motor, if it was off
+      }
+      break;
+    //Servo direction control
+    case 39: //key right
+      if (thisClient.car.steering.angle.value > 500) {
+        socket.emit("steeringSocket", thisClient.car.steering.angle.value - thisClient.car.steering.angle.increment);
+        thisClient.car.steering.angle.value -= thisClient.car.steering.angle.increment;
+      }
+      break;
+    case 37: //key left
+      if (thisClient.car.steering.angle.value < 2500) {
+        socket.emit("steeringSocket", thisClient.car.steering.angle.value + thisClient.car.steering.angle.increment);
+        thisClient.car.steering.angle.value += thisClient.car.steering.angle.increment;
+      }
+      break;
+  }
+}
