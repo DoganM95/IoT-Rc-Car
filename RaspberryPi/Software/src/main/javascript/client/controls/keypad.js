6011ceb0c5a75a9a6079dcd5bc883910829bd835
DoganM95 <doganmermertas@hotmail.de>
2020-05-17 03:21:09 +0200

    replaced by wasd and reworked

--- RaspberryPi/Software/src/main/javascript/client/controls/keypad.js
+++ RaspberryPi/Software/src/main/javascript/client/controls/keypad.js
@@ -3,40 +3,39 @@ async function handleKeydownEvent(key) {
   switch (
     key.keyCode //switch statement for pressed key
   ) {
-    case 40: //backward
-      if (thisClient.car.engine.speed.value > 0) {
-        socket.emit("motorSocket", thisClient.car.engine.speed.value - thisClient.car.engine.speed.increment);
-      }
+    //Precise control
+    //--------------------
+    case 83: // key: s, drive backward
+      thisClient.car.engine.motors.speed.value =
+        thisClient.car.engine.motors.speed.value - thisClient.car.engine.motors.speed.increment <= thisClient.settings.control.keypad.limits.y.min
+          ? thisClient.settings.control.keypad.limits.y.min
+          : thisClient.car.engine.motors.speed.value - thisClient.car.engine.motors.speed.increment;
+      socket.emit("control_engine_by_keypad", thisClient.car.engine.motors.speed.value);
       break;
-    case 38: //forward
-      if (thisClient.car.engine.speed.value < 255) {
-        socket.emit("motorSocket", thisClient.car.engine.speed.value + thisClient.car.engine.speed.increment);
-      }
+    case 87: //key: w, drive forward
+      thisClient.car.engine.motors.speed.value =
+        thisClient.car.engine.motors.speed.value + thisClient.car.engine.motors.speed.increment >= thisClient.settings.control.keypad.limits.y.max
+          ? thisClient.settings.control.keypad.limits.y.max
+          : thisClient.car.engine.motors.speed.value + thisClient.car.engine.motors.speed.increment;
+      socket.emit("control_engine_by_keypad", thisClient.car.engine.motors.speed.value);
       break;
-    case 53: //Full Speed or stop (like play pause)
-      if (thisClient.car.engine.speed.value > 0) {
-        socket.emit("motorSocket", 0); //stops motor, if motor is running
-      }
-      if (thisClient.car.engine.speed.value == 0) {
-        socket.emit("motorSocket", 255); //max-speeds motor, if it was off
-      }
+    case 65: //key: a, steer left
+      thisClient.car.steering.servos.angle.value =
+        thisClient.car.steering.servos.angle.value - thisClient.car.steering.servos.angle.increment <= thisClient.settings.controls.keypad.limits.x.min
+          ? thisClient.settings.controls.keypad.limits.x.min
+          : thisClient.car.steering.servos.angle.value - thisClient.car.steering.servos.angle.increment;
+      socket.emit("control_steering_by_keypad", thisClient.car.steering.servos.angle.value);
       break;
-    //Servo direction control
-    case 39: //key right
-      if (thisClient.car.steering.angle.value > 500) {
-        socket.emit("steeringSocket", thisClient.car.steering.angle.value - thisClient.car.steering.angle.increment);
-        thisClient.car.steering.angle.value -= thisClient.car.steering.angle.increment;
-      }
-      break;
-    case 37: //key left
-      if (thisClient.car.steering.angle.value < 2500) {
-        socket.emit("steeringSocket", thisClient.car.steering.angle.value + thisClient.car.steering.angle.increment);
-        thisClient.car.steering.angle.value += thisClient.car.steering.angle.increment;
-      }
+    case 68: //key: d, steer right
+      thisClient.car.steering.servos.angle.value =
+        thisClient.car.steering.servos.angle.value + thisClient.car.steering.servos.angle.increment >= thisClient.settings.controls.keypad.limits.x.max
+          ? thisClient.settings.controls.keypad.limits.x.max
+          : thisClient.car.steering.servos.angle.value + thisClient.car.steering.servos.angle.increment;
+      socket.emit("control_steering_by_keypad", thisClient.car.steering.servos.angle.value);
       break;
   }
-
-  //TODO: replace current keys with "w,a,s,d" for precise control and assign arrow keys to static (full value) control
-  // w = 87, a = 65, s = 83, d = 68
+  //Rough Control
+  //--------------------
+  //TODO:
   //up = 38, left = 37, down = 40, right = 39
 }
